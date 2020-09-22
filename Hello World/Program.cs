d2dd4af887a658de3863e88a51bc88e663bfb81d
Mikael Henningsson <45164189+mickeh22@users.noreply.github.com>
2020-09-22 09:56:53 +0200

    Add files via upload

---
+++ Hello World/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            Console.WriteLine("How's it going?");
+            Console.WriteLine();
+            Console.WriteLine("You know, unemployed just messing around with programming!");
+            Console.ReadKey();
+        }
+    }
+}
